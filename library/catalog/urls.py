3ff4560db44c0e6941792c2bbd2917bcfc159c3e
Your Name <you@example.com>
2024-12-05 22:50:58 +0300

    пз-18, пз-19

--- library/catalog/urls.py
+++ library/catalog/urls.py
@@ -3,4 +3,10 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('books/<int:pk>/', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors-list'),
+    path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='authors-detail'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
--- library/catalog/views.py
+++ library/catalog/views.py
@@ -1,7 +1,78 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book, Author, BookInstance
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
+# def index(request):
+#     return HttpResponse("Главная страница сайта Книжный червь!")
+
 def index(request):
-    return HttpResponse("Главная страница сайта Книжный червь!")
+    text_head = 'На нашем сайте вы можете получить книги в электронном виде'
+    # Данные о книгах и их количестве
+    books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    # Данные об экземплярах книг в БД
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'На складе')
+    num_instances_available = BookInstance.objects.filter(
+    status__exact=2).count()
+    # Данные об авторах книг
+    authors = Author.objects
+    num_authors = Author.objects.count()
+    # Словарь для передачи данных в шаблон index.html
+    context = {'text_head': text_head,
+    'books': books, 'num_books': num_books,
+    'num_instances': num_instances,
+    'num_instances_available': num_instances_available,
+    'authors': authors, 'num_authors': num_authors}
+    # передача словаря context с данными в шаблон
+    return render(request, 'catalog/index.html', context)
+
+class BookListView(ListView):
+    model = Book
+    context_object_name = 'books'
+    paginate_by = 3
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = 'book'
+
+class AuthorListView(ListView):
+    model = Author
+    paginate_by = 4
+
+class AuthorDetailView(DetailView):
+    model = Author
+
+def about(request):
+    text_head = 'Сведения о компании'
+    name = 'ООО "Интеллектуальные информационные системы"'
+    rab1 = 'Разработка приложений на основе' \
+    ' систем искусственного интеллекта'
+    rab2 = 'Распознавание объектов дорожной инфраструктуры'
+    rab3 = 'Создание графических АРТ-объектов на основе' \
+    ' систем искусственного интеллекта'
+    rab4 = 'Создание цифровых интерактивных книг, учебных пособий' \
+    ' автоматизированных обучающих систем'
+    context = {'text_head': text_head, 'name': name,
+    'rab1': rab1, 'rab2': rab2,
+    'rab3': rab3, 'rab4': rab4}
+    # передача словаря context с данными в шаблон
+    return render(request, 'catalog/about.html', context)
+
+def contact(request):
+    text_head = 'Контакты'
+    name = 'ООО "Интеллектуальные информационные системы"'
+    address = 'Москва, ул. Планерная, д.20, к.1'
+    tel = '495-345-45-45'
+    email = 'iis_info@mail.ru'
+    # Словарь для передачи данных в шаблон index.html
+    context = {'text_head': text_head,
+    'name': name, 'address': address,
+    'tel': tel,
+    'email': email}
+    # передача словаря context с данными в шаблон
+    return render(request, 'catalog/contact.html', context)
+
